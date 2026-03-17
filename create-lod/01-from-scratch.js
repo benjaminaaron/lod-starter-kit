d1324a31d61934005b9a706174318d2a5ed5b205
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-17 21:51:00 +0100

    Use hashtag for event id

--- create-lod/01-from-scratch.js
+++ create-lod/01-from-scratch.js
@@ -4,25 +4,25 @@ import { expand, prefixes } from "../utils.js"
 let store = newStore()
 
 addTriple(store,
-    expand("oddmuc2026"),
+    expand("oddmuc26"),
     expand("rdf", "type"),
-    expand("Event")
+    expand("schema", "Event")
 )
 
 addTriple(store,
-    expand("oddmuc2026"),
+    expand("oddmuc26"),
     expand("schema", "name"),
     "Open Data Day München 2026"
 )
 
 addTriple(store,
-    expand("oddmuc2026"),
+    expand("oddmuc26"),
     expand("hasDate"),
     "2026-03-21"
 )
 
 addTriple(store,
-    expand("oddmuc2026"),
+    expand("oddmuc26"),
     expand("hasSession"),
     expand("lodPotential")
 )
