96dce99dc0ba660d1f6e55d96c5bafee1c913555
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-18 09:49:20 +0100

    Add outcommented code to print csv headers

--- create-lod/02-from-local-csv-manually.js
+++ create-lod/02-from-local-csv-manually.js
@@ -9,13 +9,16 @@ const CSV_FILE = path.join(path.dirname(fileURLToPath(import.meta.url)), "input"
 const csv = Papa.parse(fs.readFileSync(CSV_FILE, "utf8"), { header: true, skipEmptyLines: true })
 let store = newStore()
 
+// console.log("headers:", csv.meta.fields)
+
 for (let row of csv.data) {
-    addTriple(store, expand(row.id), expand("rdf", "type"), expand("schema", "Event"))
-    addTriple(store, expand(row.id), expand("rdfs", "title"), row.title)
-    addTriple(store, expand(row.id), expand("schema", "startDate"), row.event_date)
-    addTriple(store, expand(row.id), expand("schema", "price"), row.ticket_price)
-    addTriple(store, expand(row.id), expand("schema", "maximumAttendeeCapacity"), row.max_attendees)
-    addTriple(store, expand(row.id), expand("schema", "eventAttendanceMode"), row.attendance_mode)
+    let id = expand(row.id)
+    addTriple(store, id, expand("rdf", "type"), expand("schema", "Event"))
+    addTriple(store, id, expand("rdfs", "title"), row.title)
+    addTriple(store, id, expand("schema", "startDate"), row.event_date)
+    addTriple(store, id, expand("schema", "price"), row.ticket_price)
+    addTriple(store, id, expand("schema", "maximumAttendeeCapacity"), row.max_attendees)
+    addTriple(store, id, expand("schema", "eventAttendanceMode"), row.attendance_mode)
 }
 
 let turtle = await storeToTurtle(store, prefixes)
