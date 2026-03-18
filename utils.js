bb7fa7ec9c1f83a23a2e7bcec8cfe7604814adc6
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-18 14:53:17 +0100

    Adjust dev and osm namespaces

--- utils.js
+++ utils.js
@@ -1,14 +1,15 @@
 import proj4 from "proj4"
 
 export const prefixes = {
-    dev: "https://open.bydata.de/oddmuc2026#",
+    dev: "https://open.bydata.de/oddmuc26#",
     rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     rdfs: "http://www.w3.org/2000/01/rdf-schema#",
     csvw: "http://www.w3.org/ns/csvw#",
     xsd: "http://www.w3.org/2001/XMLSchema#",
     schema: "https://schema.org/",
     geo: "http://www.w3.org/2003/01/geo/wgs84_pos#",
-    osmnode: "https://www.openstreetmap.org/node/"
+    osmnode: "https://www.openstreetmap.org/node/",
+    osmway: "https://www.openstreetmap.org/way/"
 }
 
 export const expand = (prefix, localName) => {
