5e8c6f402981a48abe44feea0fba6af65c5dcd1b
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-20 18:22:34 +0100

    Add example script to query remote sources

---
+++ use-lod/03-query-remote-sources.js
@@ -0,0 +1,24 @@
+import { QueryEngine } from "@comunica/query-sparql"
+
+const OPENBYDATA_ENDPOINT = "https://open.bydata.de/api/sparql"
+const engine = new QueryEngine()
+
+let query = `
+    PREFIX dcat: <http://www.w3.org/ns/dcat#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+
+    SELECT * WHERE {
+        <https://open.bydata.de/api/hub/repo/catalogues/muenchen> dcat:dataset ?dataset .
+        ?dataset a dcat:Dataset ;
+            dct:title ?title .
+    } LIMIT 10`
+
+const bindingsStream = await engine.queryBindings(query, { sources: [OPENBYDATA_ENDPOINT] })
+let rows = await bindingsStream.toArray()
+for (const row of rows) {
+    for (const key of row.keys()) {
+        const term = row.get(key)
+        console.log(`${key.value}: ${term.value}`)
+    }
+    console.log("----")
+}
