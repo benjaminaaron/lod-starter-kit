c42970864211477eb716ce6c154b1a67ba483386
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-21 12:08:21 +0100

    Add saved queries for OD portal endpoints

--- workshop-frontend/queries.js
+++ workshop-frontend/queries.js
@@ -86,5 +86,46 @@ WHERE {
     BIND((1000 * ?netMigration / ?pop2) AS ?netMigrationRatePer1000)
 }
 GROUP BY ?bezirk ?bezirkName
-ORDER BY DESC(?avgNetMigrationRatePer1000)`
+ORDER BY DESC(?avgNetMigrationRatePer1000)`,
+// -------------------------------------------------------------------
+    fromDistributionToDataset: `
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+
+SELECT * WHERE { 
+  	?dataset a dcat:Dataset ;
+        dcat:distribution <https://open.bydata.de/api/hub/repo/distributions/4808588b-a630-4c71-a1af-814707c52e79> .
+}`,
+// -------------------------------------------------------------------
+    countTotalDatasets: `
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+
+SELECT (COUNT(?dataset) as ?count) WHERE {
+	?dataset a dcat:Dataset .
+}`,
+// -------------------------------------------------------------------
+    distinctDistributionFormats: `
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+
+SELECT DISTINCT ?format WHERE {
+	?dataset a dcat:Dataset ;
+  	dcat:distribution ?distribution .
+  	?distribution dct:format ?format .
+}`,
+// -------------------------------------------------------------------
+    datasetsWithLodDistributions: `
+PREFIX dcat: <http://www.w3.org/ns/dcat#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX filetype: <http://publications.europa.eu/resource/authority/file-type/>
+
+SELECT DISTINCT ?catalogTitle ?datasetTitle WHERE { 
+	?cat a dcat:Catalog ;
+  		dct:title ?catalogTitle ;
+  		dcat:dataset ?dataset .
+  	?dataset a dcat:Dataset ;
+  		dct:title ?datasetTitle ;
+  		dcat:distribution ?distribution .
+  	?distribution dct:format ?format .
+  	VALUES ?format { filetype:JSON_LD filetype:JSONLD filetype:RDFXML filetype:TURTLE filetype:N3 }
+}`
 }
