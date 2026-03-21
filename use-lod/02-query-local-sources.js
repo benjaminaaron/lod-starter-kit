6d65a8a62594fb0e1454d41ffdd268043debdba1
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-21 12:02:38 +0100

    Add script in your-workspace

--- use-lod/02-query-local-sources.js
+++ use-lod/02-query-local-sources.js
@@ -14,7 +14,7 @@ const MAX_DIST = 200
 // Use Case: Familienfreundliches München
 
 const THIS_DIR = path.dirname(fileURLToPath(import.meta.url))
-const OUTPUT_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), "..", "create-lod", "output")
+const OUTPUT_DIR = path.join(THIS_DIR, "..", "create-lod", "output")
 const TTL_PLAYGROUNDS = path.join(OUTPUT_DIR, "240826-spielplaetze.ttl")
 const TTL_TOILETS = path.join(OUTPUT_DIR, "wc-standorte-1.ttl")
 const TTL_CAFES = path.join(OUTPUT_DIR, "osm-cafes.ttl")
---
+++ your-workspace/script.js
@@ -0,0 +1,13 @@
+import { fileURLToPath } from "url"
+import path from "path"
+import fs from "fs"
+
+// example endpoint
+const OPENBYDATA_ENDPOINT = "https://open.bydata.de/api/sparql"
+
+// example local file
+const THIS_DIR = path.dirname(fileURLToPath(import.meta.url))
+const TTL_FILE = path.join(THIS_DIR, "..", "create-lod", "output", "240826-spielplaetze.ttl")
+const fileContent = fs.readFileSync(TTL_FILE, { encoding: "utf8" })
+
+// TODO have fun :)
