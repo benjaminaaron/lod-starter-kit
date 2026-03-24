5071a3a2c6525f433fece6ffff14a591160a605f
Benjamin Degenhart <benjamin.degenhart@foerderfunke.org>
2026-03-24 14:34:26 +0100

    Move starting script to root

--- your-script.js
+++ your-script.js
@@ -7,7 +7,7 @@ const OPENBYDATA_ENDPOINT = "https://open.bydata.de/api/sparql"
 
 // example local file
 const THIS_DIR = path.dirname(fileURLToPath(import.meta.url))
-const TTL_FILE = path.join(THIS_DIR, "..", "create-lod", "output", "240826-spielplaetze.ttl")
+const TTL_FILE = path.join(THIS_DIR, "create-lod", "output", "240826-spielplaetze.ttl")
 const fileContent = fs.readFileSync(TTL_FILE, { encoding: "utf8" })
 
 // TODO have fun :)
